a3d9682fdd1674da92265f0527e7628476ca266c
amiksha125 <amikshagajbhiye@gmail.com>
2025-06-12 21:54:45 +0530

    feat: handle emoji selection

--- script.js
+++ script.js
@@ -147,11 +147,17 @@ fileCancelButton.addEventListener("click", () => {
 
 })
 
-//Initialize emoji picker
+//Initialize emoji picker and handle emoji selection
 const picker = new EmojiMart.Picker({
     theme: "light",
     skinTonePosition: "none",
     previewPosition: "none",
+    onEmojiSelect: (emoji) =>{
+        const {selectionStart: start, selectionEnd: end } =messageInput;
+        messageInput.setRangeText(emoji.native, start, end, "end");
+        messageInput.focus();
+
+    },
     onClickOutside: (e) => {
         if(e.target.id == "emoji-picker"){
             document.body.classList.toggle("show-emoji-picker");
